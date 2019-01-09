a6a5b4f5b4119844e57e2c6a835cfeacd1800dd6
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-09 14:32:49 -0800

    Allow mDNS

--- root/startup.sh
+++ root/startup.sh
@@ -53,7 +53,7 @@ iptables -t nat -A PREROUTING -p udp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERF
 iptables -t nat -A PREROUTING -p tcp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERFACE}
 # mDNS traffic handled locally
 iptables -t nat -A PREROUTING -p udp --dport 5353 -j ACCEPT -i ${INTERNAL_INTERFACE}
-iptables -t nat -A PREROUTING -p udp --dport 5353 -j ACCEPT -i ${EXTERNAL_INTERFACE}
+iptables -A INPUT -p udp --dport 5353 -j ACCEPT -i ${EXTERNAL_INTERFACE}
 # Everything else we forward
 iptables -t nat -A PREROUTING  -j DNAT --to-destination ${EXTERNAL_REMOTE_IP} -i ${INTERNAL_INTERFACE}
 iptables -t nat -A POSTROUTING -j MASQUERADE -o ${EXTERNAL_INTERFACE}
