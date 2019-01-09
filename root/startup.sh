1e5460b6a986a50ae83dfacb23d81bdfebc18fc6
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-09 09:37:04 -0800

    Firewall + routing

--- root/startup.sh
+++ root/startup.sh
@@ -4,10 +4,19 @@ HOME_INTERFACE=eth0
 INTERNAL_INTERFACE=eth1
 EXTERNAL_INTERFACE=tun0
 
+# EXTERNAL_INTERFACE (swapped from Server)
+EXTERNAL_NET=10.20.30.0
+EXTERNAL_MASK=255.255.255.248
+EXTERNAL_LOCAL_IP=10.20.30.2
+EXTERNAL_REMOTE_IP=10.20.30.1
+
 if [ ! -e /etc/openvpn/minke-client.ovpn ]; then
   echo "Missing client config"
   exit 1
 fi
+
+HOME_IP=$(ip addr show dev ${HOME_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
+INTERNAL_IP=$(ip addr show dev ${INTERNAL_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
 
 # Extract port and protocol
 remote=$(grep '^remote ' /etc/openvpn/minke-client.ovpn)
@@ -31,22 +40,31 @@ iptables -A OUTPUT -p udp --sport 68 -j ACCEPT -o ${HOME_INTERFACE}
 iptables -A INPUT  -p udp --sport 1900 -j ACCEPT -i ${HOME_INTERFACE}
 iptables -A OUTPUT -p udp --dport 1900 -j ACCEPT -o ${HOME_INTERFACE}
 # Block all other outgoing UDP traffic
-iptables -A OUTPUT -p udp -j DROP  -o ${HOME_INTERFACE}
+iptables -A OUTPUT -p udp -j DROP -o ${HOME_INTERFACE}
 # Drop anything else incoming
 iptables -A INPUT  -j DROP -i ${HOME_INTERFACE}
 
-# INTERNAL_INTERFACE -> HOME_INTERFACE
-# Block any traffic between these interfaces
-iptables -A FORWARD -j DROP -i ${INTERNAL_INTERFACE} -o ${HOME_INTERFACE}
 
 openvpn --config /etc/openvpn/minke-client.ovpn --daemon
 
+# INTERNAL_INTERFACE <-> EXTERNAL_INTERFACE
+# UPnP traffic handled locally
+iptables -t nat -A PREROUTING -p udp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERFACE}
+iptables -t nat -A PREROUTING -p tcp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERFACE}
+# Everything else we forward
+iptables -t nat -A PREROUTING  -j DNAT --to-destination ${EXTERNAL_REMOTE_IP} -i ${INTERNAL_INTERFACE}
+iptables -t nat -A POSTROUTING -j MASQUERADE -o ${EXTERNAL_INTERFACE}
+iptables -t nat -A POSTROUTING -j MASQUERADE -o ${INTERNAL_INTERFACE}
+# And we drop anything incoming we don't have a rule for
+iptables -A INPUT -j DROP -i ${EXTERNAL_INTERFACE}
 
-# UPNP
+# UPnP
+iptables -t nat -N MINIUPNPD
+iptables -t nat -A PREROUTING -i ${EXTERNAL_INTERFACE} -j MINIUPNPD
 echo "
 ext_ifname=${EXTERNAL_INTERFACE}
 listening_ip=${INTERNAL_INTERFACE}
-http_port=0
+http_port=1900
 enable_natpmp=no
 enable_upnp=yes
 min_lifetime=120
