f6f776db0a929715fa4c4aef280034ccd84399f8
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-27 16:51:26 -0800

    Simple OpenVPN client

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 FROM alpine:edge
 
-RUN apk --no-cache add openvpn easy-rsa miniupnpd nodejs npm ;\
-    rm -f /etc/openvpn/* /etc/miniupnpd/*
+RUN apk --no-cache add openvpn ;\
+    rm -f /etc/openvpn/*
 
 COPY root/ /
-
-RUN cd /mDNS ; npm install
-
-VOLUME /etc/openvpn
 
 ENTRYPOINT ["/startup.sh"]
--- root/startup.sh
+++ root/startup.sh
@@ -4,49 +4,23 @@ HOME_INTERFACE=eth0
 INTERNAL_INTERFACE=eth1
 EXTERNAL_INTERFACE=tun0
 
-# EXTERNAL_INTERFACE (swapped from Server)
-EXTERNAL_NET=10.20.30.0
-EXTERNAL_MASK=255.255.255.248
-EXTERNAL_LOCAL_IP=10.20.30.2
-EXTERNAL_REMOTE_IP=10.20.30.1
+CONFIG=/etc/openvpn/config.ovpn
+AUTH=/tmp/userpw.conf
 
-if [ ! -e /etc/openvpn/minke-client.ovpn ]; then
+if [ ! -e ${CONFIG} ]; then
   echo "Missing client config"
   exit 1
 fi
 
-HOME_IP=$(ip addr show dev ${HOME_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-INTERNAL_IP=$(ip addr show dev ${INTERNAL_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
+AUTH=/tmp/userpw.conf
 
-# Extract port and protocol
-remote=$(grep '^remote ' /etc/openvpn/minke-client.ovpn)
-remote=${remote#* * }
-PORT=${remote%% *}
-PROTO=${remote#* }
+# Create user/password file
+echo ${USER} > ${AUTH}
+echo ${PASSWORD} >> ${AUTH}
 
-# Firewall setup
-route del default
+openvpn --config ${CONFIG} --daemon --auth-user-pass ${AUTH} --script-security 2 --up "/usr/bin/env INTERNAL_INTERFACE=${INTERNAL_INTERFACE} EXTERNAL_INTERFACE=${EXTERNAL_INTERFACE} /vpn-up.sh"
 
-# HOME_INTERFACE
-# Allow traffic in and out if we've started a connection out
-iptables -A INPUT  -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT -i ${HOME_INTERFACE}
-# Allow OpenVPN traffic in and out (tcp or udp) - **ports switch from server version**
-iptables -A INPUT  -p ${PROTO} --sport ${PORT} -j ACCEPT -i ${HOME_INTERFACE}
-iptables -A OUTPUT -p ${PROTO} --dport ${PORT} -j ACCEPT -o ${HOME_INTERFACE}
-# Allow DHCP traffic in and out
-iptables -A INPUT  -p udp --dport 68 -j ACCEPT -i ${HOME_INTERFACE}
-iptables -A OUTPUT -p udp --sport 68 -j ACCEPT -o ${HOME_INTERFACE}
-# Allow UPnP traffic in and out
-iptables -A INPUT  -p udp --sport 1900 -j ACCEPT -i ${HOME_INTERFACE}
-iptables -A OUTPUT -p udp --dport 1900 -j ACCEPT -o ${HOME_INTERFACE}
-# Block all other outgoing UDP traffic
-iptables -A OUTPUT -p udp -j DROP -o ${HOME_INTERFACE}
-# Drop anything else incoming
-iptables -A INPUT  -j DROP -i ${HOME_INTERFACE}
-
-openvpn --config /etc/openvpn/minke-client.ovpn --daemon --script-security 2 --up "/usr/bin/env EXTERNAL_INTERFACE=${EXTERNAL_INTERFACE} INTERNAL_INTERFACE=${INTERNAL_INTERFACE} EXTERNAL_REMOTE_IP=${EXTERNAL_REMOTE_IP} /vpn-up.sh"
-
-trap "killall sleep openvpn miniupnpd node; exit" TERM INT
+trap "killall sleep openvpn; exit" TERM INT
 
 sleep 2147483647d &
 wait "$!"
